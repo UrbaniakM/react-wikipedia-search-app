393792a5029cb5b6ae676f44b603faecd0d38a74
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-07 00:56:23 +0200

    Add disabled prop to ReplacePhraseField component

--- src/components/ReplacePhraseField.jsx
+++ src/components/ReplacePhraseField.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { TextField } from './TextField';
 
-export const ReplacePhraseField = ({ onChange }) => {
+export const ReplacePhraseField = ({ onChange, disabled }) => {
   return (
     <TextField 
       id="replace-phrase"
       label="Replace with"
       onChange={onChange}
+      disabled={disabled}
     />
   );
 }
