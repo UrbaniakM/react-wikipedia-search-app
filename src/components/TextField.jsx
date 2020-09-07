4496cddc3d9b23b7ab91f873bce8fd326f8e30d0
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-07 18:37:03 +0200

    Store TextField value inside component

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { TextField as MuiTextField } from '@material-ui/core';
 
-export const TextField = ({ onChange, variant, ...otherProps }) => {
+export const TextField = ({ 
+  onChange, 
+  variant, 
+  value: valueProps, 
+  ...otherProps 
+}) => {
+  const [value, setValue] = React.useState(valueProps);
+
+  React.useEffect(() => {
+    setValue(valueProps);
+  }, [valueProps, setValue])
+
   const handleChange = React.useCallback((event) => {
-    onChange(event.target.value);
-  }, [onChange]);
+    const newValue = event.target.value;
+
+    setValue(newValue);
+    onChange(newValue);
+  }, [setValue, onChange]);
 
   return (
     <MuiTextField
       onChange={handleChange}
       variant={variant || 'outlined'}
+      value={value}
       {...otherProps}
     />
   )
