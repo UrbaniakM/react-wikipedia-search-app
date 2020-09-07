d1d7b2b73297cdee91a313e3648df3a0a715ec93
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-07 21:20:28 +0200

    Updates

---
+++ precache-manifest.357724910a068024c37305c5e875f4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a050cce253f3249a5dde07b1a1cee32d",
+    "url": "/react-wikipedia-search-app/index.html"
+  },
+  {
+    "revision": "493adcf2bf697f64377b",
+    "url": "/react-wikipedia-search-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31de6f7224a06d63b6ab",
+    "url": "/react-wikipedia-search-app/static/js/2.0de7f194.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-wikipedia-search-app/static/js/2.0de7f194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493adcf2bf697f64377b",
+    "url": "/react-wikipedia-search-app/static/js/main.ea533b10.chunk.js"
+  },
+  {
+    "revision": "6e518bef29f674ecd9de",
+    "url": "/react-wikipedia-search-app/static/js/runtime-main.749bae93.js"
+  }
+]);
