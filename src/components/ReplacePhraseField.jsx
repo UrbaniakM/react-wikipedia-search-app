7abc02002b359315f493b1b549d906b68116189e
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-09-07 18:37:45 +0200

    Pass value and type props to proxied TextField

--- src/components/ReplacePhraseField.jsx
+++ src/components/ReplacePhraseField.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { TextField } from './TextField';
 
-export const ReplacePhraseField = ({ onChange, disabled }) => {
+export const ReplacePhraseField = ({ onChange, value, disabled }) => {
   return (
     <TextField 
       id="replace-phrase"
       label="Replace with"
       onChange={onChange}
       disabled={disabled}
+      value={value}
+      type="text"
     />
   );
 }
--- src/components/SearchPhraseField.jsx
+++ src/components/SearchPhraseField.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TextField } from './TextField';
 
-export const SearchPhraseField = ({ onChange }) => {
+export const SearchPhraseField = ({ onChange, value }) => {
   return (
     <TextField 
       id="search-phrase"
       label="Search phrase"
       onChange={onChange}
+      value={value}
+      type="text"
     />
   );
 }
